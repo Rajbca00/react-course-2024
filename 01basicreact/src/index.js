52aa492643fba1238448b382e5e5326833f20105
Rajesh S <rajbca00@gmail.com>
2024-01-11 08:23:33 +0530

    custom react design

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: 'Click me to visit google'
+}
+
+const aReactElement = React.createElement('a', {
+    href: "https://google.com",
+    target: "_blank"
+}, 'Click me to visit google')
+
+const AnotherElement = <a href='https://google.com' target='_blank'>Click me to visit google</a>
+
+function MyApp() {
+    return (<App />)
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    AnotherElement
 );
