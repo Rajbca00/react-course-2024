31c00f5510ede833d12c0983dc72c5c8229f1db7
Rajesh S <rajbca00@gmail.com>
2024-01-11 09:01:38 +0530

    react counter and hooks

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -4,12 +4,16 @@ import Youtube from './Youtube.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
+  const username = "rajesh";
   return (
     <>
       <h1>
         Vite react app
       </h1>
-      <Youtube/>
+      <h1>
+        Vite react app {username}
+      </h1>
+      <Youtube />
     </>
   )
 }
